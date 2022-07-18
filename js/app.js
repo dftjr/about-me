c337865d7f4f55a0d138bd1515ac04e0290ae9d0
DAVID TUSIA <DAVE.TUSIA@GMAIL.COM>
2022-07-17 20:09:05 -0700

    Final line of code added. Complete

--- js/app.js
+++ js/app.js
@@ -1,231 +1,124 @@
-// "use strict";
+'use strict';
 
-// let visitorName = prompt(
-//   "Welcome to my About Me page. Please enter your name for a more personalized experience"
-// );
+let visitorName = prompt('Welcome to my About Me page. Please enter your name for a more personalized experience');
 
-// alert(
-//   "Welcome " +
-//     visitorName +
-//     " to my About Me page. I want to make this fun and interactive.\n\nPlease read and memorize the facts about myself below. You will be given a short quiz right after!\n\n- My name is David Tusia\n- I am 31 years old\n- I grew up in Connecticut\n- I have two German Shepherds named Luna and Nyx\n- I love playing video games\n- I drive a Chevy Cobalt"
-// );
+alert('Welcome ' + visitorName + ' to my About Me page. I want to make this fun and interactive.\n\nPlease read and memorize the facts about myself below. You will be given a short quiz right after!\n\n- My name is David Tusia\n- I am 31 years old\n- I grew up in Connecticut\n- I have two German Shepherds named Luna and Nyx\n- I love playing video games\n- I drive a Chevy Cobalt');
 
-// //this is to keep count of the correctly answered questions. counter++ adds 1 to the starting value/current value.
-// let counter = 0;
+let counter = 0;
+let messageAttempts = 'Attempts Remaining: ';
 
-// //.toLowerCase() makes all entries lower case, .trim() takes away any spaces before and after the value submitted to the prompt
+alert('Time to play a little guessing game...');
 
-function project() {
-  let homeState = prompt(
-    "Did I grow up in Washington State?\n(Please enter: y/n or yes/no)"
-  );
-  if (homeState === "no" || homeState === "n") {
-    //console.log('Input: ' + homeState + ' Output: Correct!');
-    alert("Correct!");
-    counter++;
-  } else if (homeState) {
-    //console.log('Input: ' + homeState + ' Output: Incorrect!');
-    alert("Incorrect... I mean, it was a 50/50 shot right?");
+quiz1();
+
+function quiz1() {
+  let questions = ['Did I grow up in Washington State?', 'Were my dogs both German Shepherds?', 'Do I hate video games?', 'Is the brand of car I drive a Chevy?', 'Am I older than 25?'];
+  let answers = ['n', 'y', 'n', 'y', 'y'];
+  let visitorAnswerQ1;
+  for (let i = 0; i < questions.length && answers.length; i++) {
+    visitorAnswerQ1 = prompt(questions[i]).toLowerCase().trim();
+    console.log(visitorAnswerQ1);
+    if (visitorAnswerQ1 === null) {
+      break;
+    }
+    if (visitorAnswerQ1 === 'yes') {
+      visitorAnswerQ1 = 'y';
+    }
+    if (visitorAnswerQ1 === 'no') {
+      visitorAnswerQ1 = 'n';
+    }
+    if (false === answers.includes(visitorAnswerQ1)) {
+      alert('Please enter a valid entry');
+      i--;
+    }
+    else if (visitorAnswerQ1 === answers[i]) {
+      alert('You did it!');
+      counter++;
+    }
+    else {
+      alert('Incorrect!');
+    }
   }
 }
 
-function question2() {
-  let question2 = prompt(
-    "Were my dogs both German Shepherds?\n(Please enter: y/n or yes/no)"
-  )
-    .toLowerCase()
-    .trim();
+alert('Let us see how you are doing so far...\n' + counter + ' / 7 questions correct so far. Keep up the good work!');
 
-  if (question2 === "yes" || question2 === "y") {
-    //console.log('Input: ' + question2 + ' Output: Correct!');
-    alert("Correct!");
-    counter++;
-  } else if (question2 === "no" || question2 === "n") {
-    //console.log('Input: ' + mypet + ' Output: Incorrect!');
-    alert(
-      "Incorrect... I let them know you called them poodles, they are not thrilled!"
-    );
-  } else {
-    alert("I do not recognize that entry, we can move on.");
+quiz2();
+
+function quiz2() {
+  let visitorAnswerQ2 = 0;
+  let higherOrLower = '';
+  let guessesRemainingQ2 = 4;
+  let possibleAnswers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let myNumber = Math.ceil(Math.random() * 10);
+  while (guessesRemainingQ2) {
+    visitorAnswerQ2;
+    visitorAnswerQ2 = parseInt(prompt(`${messageAttempts} ${guessesRemainingQ2}\n${higherOrLower}\n\nGuess a number between 1 - 10: `), 10);
+    if (visitorAnswerQ2 === null) {
+      break;
+    }
+    if (false === possibleAnswers.includes(visitorAnswerQ2)) {
+      alert('Please enter a valid entry');
+    }
+    else if (visitorAnswerQ2 === myNumber) {
+      alert('You did it!');
+      counter++;
+      break;
+    }
+    else {
+      guessesRemainingQ2--;
+      alert('Incorrect!');
+    }
+    if (false === possibleAnswers.includes(visitorAnswerQ2)) {
+      console.log('null');
+    }
+    else if (visitorAnswerQ2 > myNumber) {
+      higherOrLower = 'You guessed: ' + visitorAnswerQ2 + ' The number is Lower!';
+    }
+    else {
+      higherOrLower = 'You guessed: ' + visitorAnswerQ2 + ' The number is Higher!';
+    }
+    if (guessesRemainingQ2 === 0) {
+      return alert('Oh no, you ran out of attempts! The number was: ' + myNumber);
+    }
   }
 }
-question2();
 
-function question3() {
-  let question3 = prompt(
-    "Do I hate video games?\n(Please enter: y/n or yes/no)"
-  )
-    .toLowerCase()
-    .trim();
+alert('So far you have a total of ' + counter + ' /7 questions correct. Keep going!');
 
-  if (question3 === "no" || question3 === "n") {
-    //console.log('Input: ' + question3 + ' Output: Correct!');
-    alert("Correct!");
-    counter++;
-  } else if (question3 === "yes" || question3 === "y") {
-    //console.log('Input: ' + question 3 + ' Output: Incorrect!');
-    alert("Incorrect... I will forever love them!");
-  } else {
-    alert("I do not recognize that entry, we can move on.");
+alert('Hmmm, lets make this a little bit tougher... do you like Geography?');
+
+quiz3();
+
+function quiz3() {
+  let visitorAnswerQ3;
+  let guessesRemainingQ3 = 6;
+  let states = ['nebraska','nevada','new hampshire','new jersey','new mexico','new york','north carolina','north dakota'];
+  let questions = ['Name a state that starts with the letter N: '];
+  while (guessesRemainingQ3) {
+    for (let i = 0; i < states.length; i++) {
+      visitorAnswerQ3 = prompt(`${messageAttempts} ${guessesRemainingQ3}\n\n${questions[0]}`).toLowerCase().trim();
+      if (visitorAnswerQ3 === null) {
+        break;
+      }
+      if (true === states.includes(visitorAnswerQ3)) {
+        alert('You did it!');
+        counter++;
+        return guessesRemainingQ3 === 0;
+      }
+      if (visitorAnswerQ3 === '') {
+        alert('Please enter a valid entry');
+      }
+      else if (visitorAnswerQ3 !== states.includes(visitorAnswerQ3)) {
+        alert('Incorrect!');
+        guessesRemainingQ3--;
+      }
+      if (guessesRemainingQ3 === 0) {
+        return alert(`Oh no, you ran out of attempts! The states that you could've chosen were: \n\n${states}`);
+      }
+    }
   }
 }
-question3();
 
-function question4() {
-  let question4 = prompt(
-    "Is the brand of car I drive a Chevy?\n(Please enter: y/n or yes/no)"
-  )
-    .toLowerCase()
-    .trim();
 
-  if (question4 === "yes" || question4 === "y") {
-    //console.log('Input: ' + aquestion4 + ' Output: Correct!');
-    alert("Correct!");
-    counter++;
-  } else if (question4 === "no" || question4 === "n") {
-    //console.log('Input: ' + anyCar + ' Output: Incorrect!');
-    alert("Incorrect... you must be a Ford fan.");
-  } else {
-    alert("I do not recognize that entry, we can move on.");
-  }
-}
-question4();
-
-("use strict");
-
-function question5() {
-  let question5 = prompt("Am I older than 25?\n(Please enter: y/n or yes/no)")
-    .toLowerCase()
-    .trim();
-
-  if (question5 === "yes" || question5 === "y") {
-    //console.log('Input: ' + question5 + ' Output: Correct!');
-    alert("Correct!");
-    counter++;
-  } else if (question5 === "no" || question5 === "n") {
-    //console.log('Input: ' + myAge + ' Output: Incorrect!');
-    alert("Incorrect... but I do appreciate the compliment.");
-  } else {
-    alert("I do not recognize that entry, we can move on.");
-  }
-}
-question5();
-
-alert(
-  "Let us see how you are doing so far...\n" +
-    counter +
-    " / 7 questions correct so far. Keep up the good work!"
-);
-alert("Time to play a little guessing game...");
-
-// let guessesNumber = 4;
-// let myNumber = Math.ceil(Math.random() * 10);
-// let visitorNumber;
-
-// while (guessesNumber) {
-//   if (visitorNumber === undefined) {
-//     visitorNumber = parseInt(
-//       prompt(
-//         `Attempts Remaining: ${guessesNumber}\n\nGuess the number on my mind, it is between 1-10:`,
-//         "0"
-//       ),
-//       10
-//     );
-//     guessesNumber--;
-//   }
-//   if (visitorNumber === myNumber) {
-//     // console.log('You did it!');
-//     alert("You did it!");
-//     counter++;
-//     break;
-//   }
-//   if (visitorNumber === 0 || null) {
-//     alert("I do not recognize that entry, we can move on.");
-//     break;
-//   }
-//   if (visitorNumber < myNumber) {
-//     // console.log('Hint: Higher');
-//     visitorNumber = parseInt(
-//       prompt(
-//         `Attempts Remaining: ${guessesNumber}\n\nNot quite, you entered: ` +
-//           visitorNumber`\nhere is a hint: Higher\n\n Guess a number:`,
-//         "0"
-//       ),
-//       10
-//     );
-//     guessesNumber--;
-//   }
-//   if (visitorNumber > myNumber) {
-//     // console.log('Hint: Lower');
-//     visitorNumber = parseInt(
-//       prompt(
-//         `Attempts Remaining: ${guessesNumber}\n\nNot quite, you entered: ` +
-//           visitorNumber`\nhere is a hint: Lower\n\n Guess a number`,
-//         "0"
-//       ),
-//       10
-//     );
-//     guessesNumber--;
-//   }
-// }
-
-// if (guessesNumber === 0) {
-//   // console.log('Sorry, no more trys left');
-//   alert("Sorry, no more trys left!\nThe number was: " + myNumber);
-// }
-
-// alert(
-//   "So far you have a total of " + counter + " /7 questions correct. Keep going!"
-// );
-// alert("Hmmm, lets make this a little bit tougher... do you like Geography?");
-
-// let visitorAnswer = "";
-// let i;
-// let guessesArray = 6;
-
-// const states = [
-//   "nebraska",
-//   "nevada",
-//   "new hampshire",
-//   "new jersey",
-//   "new mexico",
-//   "new york",
-//   "north carolina",
-//   "north dakota",
-// ];
-
-// do {
-//   visitorAnswer = prompt(
-//     `Attempts Remaining: ${guessesArray}\n\nEnter US State that begins with the letter N:`
-//   )
-//     .toLowerCase()
-//     .trim();
-//   for (i = 0; i < states.length; i++) {
-//     if (states[i] === visitorAnswer) {
-//       alert(
-//         "Good Job!\n\nHere is a list of all the states that would have been accepted as well:\n\n" +
-//           states
-//       );
-//       counter++;
-//       break;
-//     }
-//   }
-//   if (visitorAnswer === states[i]) {
-//     break;
-//   }
-//   guessesArray--;
-// } while (guessesArray);
-
-// if (guessesArray === 0) {
-//   alert(
-//     "Sorry, you have used up all your attempts. That is going to hurt the leaderboards!"
-//   );
-// }
-
-// alert(
-//   "You really are good at this, just look at your score " +
-//     counter +
-//     " /7! That is incredible!\n\nWell until I make more games, please enjoy the rest of my website " +
-//     visitorName +
-//     "!"
-// );
+alert('You really are good at this, just look at your score ' + counter + ' /7! That is incredible!\n\nWell until I make more games, please enjoy the rest of my website ' + visitorName + '!');
